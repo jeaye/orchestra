c5eab4ebe6bbca806958aee67ec1a4a799798b4c
jeaye <contact@jeaye.com>
2017-03-28 23:31:28 -0700

    Enable reflection warnings

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]]
+  :global-vars {*warn-on-reflection* true}
   :profiles {:uberjar {:aot :all}})
