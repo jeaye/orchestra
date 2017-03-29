b21e2ef91a952bd34f6c71e3aa11899c2b15f030
jeaye <contact@jeaye.com>
2017-03-28 23:34:07 -0700

    Update project

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject orchestra "0.1.0"
-  :description "Better clojure.spec instrumentation"
+  :description "Complete clojure.spec instrumentation"
   :url "https://github.com/jeaye/orchestra"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
