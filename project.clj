41c37eecc90bf2f7428014a9ff720de22c65994f
jeaye <contact@jeaye.com>
2017-03-29 23:08:47 -0700

    Add codecov support

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]]
+  :plugins [[lein-cloverage "1.0.9"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:uberjar {:aot :all}})
