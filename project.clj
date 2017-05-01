663a997e9c4596cbf1f338504070c8067cf01e5c
jeaye <contact@jeaye.com>
2017-05-01 16:22:44 -0700

    Work on 0.3 snapshot

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject orchestra "0.2.0-SNAPSHOT"
+(defproject orchestra "0.3.0-SNAPSHOT"
   :description "Complete instrumentation for clojure.spec"
   :url "https://github.com/jeaye/orchestra"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]]
   :plugins [[lein-cloverage "1.0.9"]]
-  :global-vars {*warn-on-reflection* true}
-  :profiles {:uberjar {:aot :all}})
+  :global-vars {*warn-on-reflection* true})
