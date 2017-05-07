2b82a83df7b2228976e86f214198647b7127dfec
jeaye <contact@jeaye.com>
2017-05-06 23:08:18 -0700

    Clean up some cljs bits

--- project.clj
+++ project.clj
@@ -5,33 +5,21 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha16" :scope "provided"]
                  [org.clojure/clojurescript "1.9.521" :scope "provided"]
+                 [org.clojure/spec.alpha "0.1.108" :scope "provided"]
                  [lein-doo "0.1.7"]]
   :plugins [[lein-cloverage "1.0.9"]
             [lein-cljsbuild "1.1.6"]
             [lein-figwheel "0.5.10"]
             [lein-doo "0.1.7"]]
-  :global-vars {*warn-on-reflection* true}
   :hooks [leiningen.cljsbuild]
-  :profiles {:clj {:dependencies [[org.clojure/spec.alpha "0.1.108"]]
-                   :source-paths ["src/clj/" "test/"]}
-             :cljs {:dependencies []}}
+  :global-vars {*warn-on-reflection* true}
+  :source-paths ["src/clj/" "test/"]
   :cljsbuild {:test-commands {"test" ["lein" "doo" "phantom" "test" "once"]}
-              ; TODO: Clean this up with a for
-              :builds {:dev {:source-paths ["src/cljs/"]
-                             :figwheel true
-                             :compiler
-                             {:main orchestra.test
-                              :optimizations :none
-                              :output-to "target/dev.js"}}
-                       :prod {:source-paths ["src/cljs/"]
-                              :compiler
-                              {:main orchestra.test
-                               :optimizations :prod
-                               :output-to "target/advanced.js"
-                               :pretty-print true}}
-                       :test {:source-paths ["src/cljs/" "test/"]
+              ; TODO: Test also with optimizations
+              :builds {:test {:source-paths ["src/cljs/" "test/"]
                               :compiler
                               {:main runner.doo
                                :optimizations :none
+                               :output-dir "target/test"
                                :output-to "target/test.js"}}}}
   :deploy-repositories [["releases" :clojars]])
