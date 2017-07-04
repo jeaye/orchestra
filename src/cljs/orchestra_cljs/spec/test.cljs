6979a502974adc59ff1b9171291f018425d5fd33
jeaye <contact@jeaye.com>
2017-07-03 22:16:54 -0700

    Ease up on overzealous trimming

--- src/cljs/orchestra_cljs/spec/test.cljs
+++ src/cljs/orchestra_cljs/spec/test.cljs
@@ -20,6 +20,19 @@
     [clojure.test.check :as stc]
     [clojure.test.check.properties]))
 
+(defn distinct-by
+  ([f coll]
+   (let [step (fn step [xs seen]
+                (lazy-seq
+                  ((fn [[x :as xs] seen]
+                     (when-let [s (seq xs)]
+                       (let [v (f x)]
+                         (if (contains? seen v)
+                           (recur (rest s) seen)
+                           (cons x (step (rest s) (conj seen v)))))))
+                    xs seen)))]
+     (step coll #{}))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; instrument ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def ^:private ^:dynamic *instrument-enabled*
