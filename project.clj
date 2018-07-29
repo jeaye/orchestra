64b5117b4e027849c0fc5ae02b6119171806aafd
jeaye <contact@jeaye.com>
2018-07-29 11:04:36 -0700

    Upgrade all deps

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/jeaye/orchestra"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta1" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.946" :scope "provided"]
-                 [org.clojure/spec.alpha "0.1.143" :scope "provided"]]
-  :plugins [[lein-cloverage "1.0.10"]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.339" :scope "provided"]
+                 [org.clojure/spec.alpha "0.2.168" :scope "provided"]]
+  :plugins [[lein-cloverage "1.0.11"]
             [lein-cljsbuild "1.1.7"]
-            [lein-figwheel "0.5.14"]
-            [lein-doo "0.1.8"]
+            [lein-figwheel "0.5.16"]
+            [lein-doo "0.1.10"]
             [com.jakemccrary/lein-test-refresh "0.23.0"]]
   :global-vars {*warn-on-reflection* true}
   :source-paths ["src/clj/" "src/cljc/" "src/cljs/"]
@@ -23,7 +23,7 @@
                          :output-dir "target/test"
                          :output-to "target/test.js"}}}}
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[lein-doo "0.1.8"]]
+             :dev {:dependencies [[lein-doo "0.1.10"]]
                    :source-paths ["test/clj/" "test/cljc/"]
                    :cljsbuild {:builds {:app
                                         {:source-paths ["test/cljs/" "test/cljc/"]
