a9fd36af3d7183bb16375ca13739d7f54431eb0d
jeaye <contact@jeaye.com>
2018-12-06 23:33:02 -0800

    Run expound tests

--- test/cljs/orchestra_cljs/test.cljs
+++ test/cljs/orchestra_cljs/test.cljs
@@ -1,7 +1,9 @@
 (ns orchestra-cljs.test
   (:require [doo.runner :refer-macros [doo-tests]]
             orchestra.core-test
+            orchestra.expound-test
             orchestra.many-fns))
 
 (doo-tests 'orchestra.core-test
+           'orchestra.expound-test
            'orchestra.many-fns)
