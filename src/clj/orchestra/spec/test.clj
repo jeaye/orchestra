89f2968e13bd2011ebe3511e1ea6b6a6b96377ce
timsgardner <timsgardner@gmail.com>
2019-01-26 01:14:04 -0500

    instrument ex-infos carry the var

--- src/clj/orchestra/spec/test.clj
+++ src/clj/orchestra/spec/test.clj
@@ -115,7 +115,7 @@ failure in instrument."
                                        (#'s/->sym v)
                                        " did not conform to spec:\n"
                                        (with-out-str (s/explain-out ed)))
-                                    ed)))
+                                    (assoc ed :var v))))
                          conformed))))]
     (fn
       [& args]
